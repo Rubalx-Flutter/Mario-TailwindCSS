b39f0f9243f94f6b4f5d936221f608e4a70b8798
Capi <54859381+Rubalx-Flutter@users.noreply.github.com>
2024-05-06 11:39:04 -0600

    Compilación Tailwind con CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    purge: [ './public/**/*.html',   ],
-    darkMode: false, // or 'media' or 'class'
+    content: [ './public/**/*.{html,css}' ],
+    darkMode: 'media', // or 'media' or 'class'
     theme: {
         extend: { }
     },
